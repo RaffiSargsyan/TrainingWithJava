9e4faa87106eaf362e1b7c5e62f84df52fb09164
Raffi Sargsyan <raffisargsyan98@gmail.com>
2023-08-09 23:41:30 +0400

    Training

--- src/lessons/lesson1/Example.java
+++ src/lessons/lesson1/Example.java
@@ -8,10 +8,10 @@ public class Example {
         a = 1;
         a = -1;
         System.out.println(a);
-        long n = 23231212234L;
+        long n = 23231212234L; //about long and word l
         System.out.println(n);
         float z = 43.2F;
-        double c = 23.3;
+        double c = 23.3; // about double word f last on float
         boolean v = true;
         System.out.println(v);
         v = false;
---
+++ src/lessons/lesson1/Example2.java
@@ -0,0 +1,7 @@
+package lessons.lesson1;
+
+public class Example2 {
+    public static void main(String[] args) {
+
+    }
+}

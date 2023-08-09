a365e3811bbb30859265caf0200ad31f1ed8de53
Raffi Sargsyan <raffisargsyan98@gmail.com>
2023-08-09 23:49:12 +0400

    Training2

--- src/lessons/lesson1/Example.java
+++ src/lessons/lesson1/Example.java
@@ -8,24 +8,27 @@ public class Example {
         a = 1;
         a = -1;
         System.out.println(a);
-        long n = 23231212234L; //about long and word l
+        long n = 23231212234L;  //about long and word l
         System.out.println(n);
         float z = 43.2F;
-        double c = 23.3; // about double word f last on float
+        double c = 23.3;  // about double word f last on float
+
         boolean v = true;
         System.out.println(v);
         v = false;
         System.out.println(v);
+
         char q = '$';
         char qq = '\u003F';
         System.out.println(q);
         System.out.println(qq);
-        System.out.println(4);
+
         String s = "Dear";
         System.out.println(s);
         String firstName = "Raffi";
         String lastName = "Sargsyan";
         System.out.println(firstName + " " + lastName);
+        //  about String for +
         int k = 12244;
         System.out.println("k= " + k);
     }
